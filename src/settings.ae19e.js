6c5d34d1f301ada2af848d0744815837cca8b350
Amandeep Singh <77589604+amansandhu2718@users.noreply.github.com>
2024-09-22 19:53:36 +0530

    panel ui and architecture

--- src/settings.ae19e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Lobby.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"3ca0a",main:"541ff"}};
