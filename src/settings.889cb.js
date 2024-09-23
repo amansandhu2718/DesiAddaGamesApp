563b539baaca79ee8b5e1094dfda95d5999c2d12
Amandeep Singh <77589604+amansandhu2718@users.noreply.github.com>
2024-09-23 22:19:11 +0530

    swipe test

---
+++ src/settings.889cb.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Lobby.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"36730",main:"703d2"}};
